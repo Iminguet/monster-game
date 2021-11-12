c885a789b7b6cb33162e2a7ebafc64729c4a17e1
Isra <i.minguet@hotmail.com>
2021-11-12 09:33:55 +0100

    añadidos diagramas de flujo, ampliados y corregidos

--- gameFinale.js
+++ gameFinale.js
@@ -1,7 +1,16 @@
 // Variables globales ( ataque)
-
+let ataque = parseInt(Math.random() * 100);
 // Objeto monster
+let monster = {
+  name: mnombre,
+  health: 100,
+};
 
 // objeto player
+let player = {
+  name: nombre,
+  health: 100,
+  potion: 2,
+};
 
 // objeto estadisticas
---
+++ test_field.js
@@ -0,0 +1,14 @@
+suma = () => {
+  let resultadosum = 5 + 5;
+
+  resta = () => {
+    let resultadores = 2 - 3;
+    return resultadores;
+  };
+
+  round = (a, b) => {
+    console.log(a - b);
+  };
+  round(resultadosum, resultadores);
+};
+suma();
